28aa0cf42e04261b4fdadecdde902810d37fde62
Keith Hamm <hkhamm@gmail.com>
2015-11-13 14:41:30 -0800

    remove def main() move to if __name__ == "__main__":

--- main.py
+++ main.py
@@ -445,27 +445,7 @@ def format_arrow_date_time(date_time):
         return "(bad date)"
 
 
-def main():
-    # App is created above so that it will
-    # exist whether this is 'main' or not
-    # (e.g., if we are running in a CGI script)
-    import uuid
-
-    app.secret_key = str(uuid.uuid4())
-    app.debug = CONFIG.DEBUG
-    app.logger.setLevel(logging.DEBUG)
-    # We run on localhost only if debugging,
-    # otherwise accessible to world
-    if CONFIG.DEBUG:
-        # Reachable only from the same computer
-        app.run(port=CONFIG.PORT)
-    else:
-        # Reachable from anywhere
-        app.run(port=CONFIG.PORT, host="0.0.0.0")
-
-
 if __name__ == "__main__":
-    # main()
     import uuid
 
     app.secret_key = str(uuid.uuid4())
@@ -475,4 +455,3 @@ if __name__ == "__main__":
         app.run(port=CONFIG.PORT)
     else:
         app.run(port=CONFIG.PORT, host="0.0.0.0")
-
